81ef0bcb0fc2d2942f92cadcf11fd3008ad5db65
Peter Sarll <petersarll@gmail.com>
2019-12-16 14:28:09 -0800

    removed .env from file

---
+++ twitter_bot.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const Twitter = require('twitter');
+
+const client = new Twitter({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+ 
+  client.post('statuses/update', {status: 'This message was sent from my terminal!'},  function(error, tweet, response) {
+    if(error) throw error;
+    console.log(tweet);  // Tweet body.
+    console.log(response);  // Raw response object.
+  });
+
+
