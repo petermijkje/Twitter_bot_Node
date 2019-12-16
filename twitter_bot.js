a4b1a2efba3c683e8922f9401cfbe389cef86f99
Peter Sarll <petersarll@gmail.com>
2019-12-16 14:43:21 -0800

    added comments

--- twitter_bot.js
+++ twitter_bot.js
@@ -1,17 +1,19 @@
 require('dotenv').config()
 const Twitter = require('twitter');
 
+//add developer keys. 
 const client = new Twitter({
   consumer_key: process.env.CONSUMER_KEY,
   consumer_secret: process.env.CONSUMER_SECRET,
   access_token_key: process.env.ACCESS_TOKEN_KEY,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
- 
-  client.post('statuses/update', {status: 'This message was sent from my terminal!'},  function(error, tweet, response) {
-    if(error) throw error;
-    console.log(tweet);  // Tweet body.
-    console.log(response);  // Raw response object.
-  });
+
+//status is the body of your message
+client.post('statuses/update', {status: 'This message was sent from my terminal!'},  function(error, tweet, response) {
+  if(error) throw error;
+  console.log(tweet);  // Tweet body.
+  console.log(response);  // Raw response object.
+});
 
 
